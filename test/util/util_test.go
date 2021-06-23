a734a1ed0fc79762a93829d0d08bf809527c6ece
Jack Jia <jack@bluvec.com>
2021-06-22 17:35:10 -0700

    fix util namespace

--- test/util/util_test.go
+++ test/util/util_test.go
@@ -2,24 +2,22 @@ package util
 
 import (
 	"testing"
-
-	"github.com/bluvec/go-libjpeg/test/util"
 )
 
 func TestOpenFile(t *testing.T) {
-	for _, file := range util.SubsampledImages {
-		util.OpenFile(file)
+	for _, file := range SubsampledImages {
+		OpenFile(file)
 	}
 }
 
 func TestReadFile(t *testing.T) {
-	for _, file := range util.SubsampledImages {
-		util.ReadFile(file)
+	for _, file := range SubsampledImages {
+		ReadFile(file)
 	}
 }
 
 func TestCreateFile(t *testing.T) {
-	f := util.CreateFile("util_test")
+	f := CreateFile("util_test")
 	f.Write([]byte{'o', 'k'})
 	f.Close()
 }
